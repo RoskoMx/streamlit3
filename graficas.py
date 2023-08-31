a5f7ea2e5ef73e924323348d443348cc08186e4e
LilianaC <liliana.castanon@gmail.com>
2023-08-30 22:14:43 -0600

    Update graficas.py

--- graficas.py
+++ graficas.py
@@ -1,27 +1,16 @@
 import pandas as pd
 import streamlit as st
 
-
+st.title("Gráficas de Pandas")
 st.text_input("Your name", key="name")
 
 # You can access the value at any point with:
 st.session_state.name
 
-
-
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
-
-    chart_data
-
-st.title("Gráficas de Pandas")
-
-
 df = pd.read_csv('https://raw.githubusercontent.com/LilianaC/streamlit3/main/Datos%20F1%20Dutch%20GP%20-%20Sheet1.csv')
 
-df
+if st.checkbox('Show dataframe'):
+    df
 
 st.line_chart(
     df,
