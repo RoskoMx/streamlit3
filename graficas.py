f02c90d830c345c3936989baf162f61baa97ee87
LilianaC <liliana.castanon@gmail.com>
2023-08-30 21:37:08 -0600

    Update graficas.py

--- graficas.py
+++ graficas.py
@@ -2,7 +2,6 @@ import pandas as pd
 import streamlit as st
 
 st.title("Gráficas de Pandas")
-#st.header('Datos de la fórmula 1', divider='rainbow')
 
 df = pd.read_csv('https://raw.githubusercontent.com/LilianaC/streamlit3/main/Datos%20F1%20Dutch%20GP%20-%20Sheet1.csv')
 
