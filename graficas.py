2652d2f972ee5c7d15e68794440d976ccecdd172
LilianaC <liliana.castanon@gmail.com>
2023-08-30 22:06:06 -0600

    Update graficas.py

--- graficas.py
+++ graficas.py
@@ -3,6 +3,22 @@ import streamlit as st
 
 st.title("Gráficas de Pandas")
 
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+
 df = pd.read_csv('https://raw.githubusercontent.com/LilianaC/streamlit3/main/Datos%20F1%20Dutch%20GP%20-%20Sheet1.csv')
 
 st.line_chart(
